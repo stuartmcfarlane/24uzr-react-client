68e38fddb07eddbcb938874a14eae6271edf85c1
Stuart McFarlane <stuart@mcfarlane.nl>
2019-09-18 15:01:58 +0200

    start and end selection

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -52,46 +52,50 @@ class Overview extends React.Component {
               </React.Fragment>
             : <ShipSelector ships={this.state.ships} shipSelected={this.shipSelected}/>
           }
-          {this.state.startBouy
-              ? <React.Fragment>
-                  <h3>From</h3>
-                  <p>{this.state.startBouy.name}</p>
-                  <p>{this.state.startBouy.location.lat.toFixed(4)} N</p>
-                  <p>{this.state.startBouy.location.lon.toFixed(4)} E</p>
-                  <button onClick={this.setStartBouy.bind(this, null)}>X</button>
-                </React.Fragment>
-              : ''
-          }
-          {this.state.endBouy
-              ? <React.Fragment>
-                  <h3>To</h3>
-                  <p>{this.state.endBouy.name}</p>
-                  <p>{this.state.endBouy.location.lat.toFixed(4)} N</p>
-                  <p>{this.state.endBouy.location.lon.toFixed(4)} E</p>
-                  <button onClick={this.setEndBouy.bind(this, null)}>X</button>
-                </React.Fragment>
-              : ''
-          }
-          {this.state.selectedBouy
-              ? <React.Fragment>
-                  <h3>Selected bouy</h3>
-                  <p>{this.state.selectedBouy.name}</p>
-                  <p>{this.state.selectedBouy.location.lat.toFixed(4)} N</p>
-                  <p>{this.state.selectedBouy.location.lon.toFixed(4)} E</p>
-                  <button onClick={this.setStartBouy.bind(this, this.state.selectedBouy)}>From</button>
-                  <button onClick={this.setEndBouy.bind(this, this.state.selectedBouy)}>To</button>
-                </React.Fragment>
-              : ''
-          }
-          {this.state.hoveredBouy
-              ? <React.Fragment>
-                  <h3>Bouy</h3>
-                  <p>{this.state.hoveredBouy.name}</p>
-                  <p>{this.state.hoveredBouy.location.lat.toFixed(4)} N</p>
-                  <p>{this.state.hoveredBouy.location.lon.toFixed(4)} E</p>
-                </React.Fragment>
-              : ''
-          }
+          <div className="row">
+            {this.state.startBouy
+                ? <div style={bouyStyle}>
+                    <h3>From</h3>
+                    <p>{this.state.startBouy.name}</p>
+                    <p>{this.state.startBouy.location.lat.toFixed(4)} N</p>
+                    <p>{this.state.startBouy.location.lon.toFixed(4)} E</p>
+                    <button onClick={this.setStartBouy.bind(this, null)}>X</button>
+                  </div>
+                : ''
+            }
+            {this.state.endBouy
+                ? <div style={bouyStyle}>
+                    <h3>To</h3>
+                    <p>{this.state.endBouy.name}</p>
+                    <p>{this.state.endBouy.location.lat.toFixed(4)} N</p>
+                    <p>{this.state.endBouy.location.lon.toFixed(4)} E</p>
+                    <button onClick={this.setEndBouy.bind(this, null)}>X</button>
+                  </div>
+                : ''
+            }
+          </div>
+          <div className="row">
+            {this.state.selectedBouy
+                ? <div style={bouyStyle}>
+                    <h3>Selected bouy</h3>
+                    <p>{this.state.selectedBouy.name}</p>
+                    <p>{this.state.selectedBouy.location.lat.toFixed(4)} N</p>
+                    <p>{this.state.selectedBouy.location.lon.toFixed(4)} E</p>
+                    <button onClick={this.setStartBouy.bind(this, this.state.selectedBouy)}>From</button>
+                    <button onClick={this.setEndBouy.bind(this, this.state.selectedBouy)}>To</button>
+                  </div>
+                : ''
+            }
+            {this.state.hoveredBouy
+                ? <div style={bouyStyle}>
+                    <h3>Bouy</h3>
+                    <p>{this.state.hoveredBouy.name}</p>
+                    <p>{this.state.hoveredBouy.location.lat.toFixed(4)} N</p>
+                    <p>{this.state.hoveredBouy.location.lon.toFixed(4)} E</p>
+                  </div>
+                : ''
+            }
+          </div>
         </div>
         <div className="main-panel" style={mainPanelStyle}>
           <Map bouySelected={this.bouySelected}
@@ -123,6 +127,12 @@ const mainPanelStyle = {
   float: 'left',
   width: '80%',
   height: '100%',
+};
+
+const bouyStyle = {
+  display: 'inline-block',
+  width: '50%',
+  float: 'left'
 };
 
 export default Overview;
